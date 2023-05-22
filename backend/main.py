4ce2b1d7b2ad29ed6dbde7ece5bb171e8c00ac36
TatChayanit <89236840+TatChayanit@users.noreply.github.com>
2023-05-22 19:57:46 +1000

    add twitter shortages

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ import uvicorn
 
 from process_mastodon_data import process_mastodon
 from views import rent_view
+from get_twitter_shortage import tweet_shortages
 
 app = FastAPI()
 
@@ -10,8 +11,8 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/mastodon_worldcloud")
-async def wordcloud():
+@app.get("/mastodon_wordcloud")
+async def mastodon_wordcloud():
     final_words = process_mastodon()
 
     return final_words
@@ -20,6 +21,12 @@ async def wordcloud():
 async def rent():
     result = rent_view()
     return result
+
+@app.get("/twitter_wordcloud")
+async def twitter_wordcloud():
+    final_words = tweet_shortages()
+    
+    return final_words
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
