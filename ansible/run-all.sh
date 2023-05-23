e50dd9cb726d660ebd1e13dd6cffdd587e08989f
meermeerkat <44202037+meermeerkat@users.noreply.github.com>
2023-05-23 13:50:41 +1000

    latest ansible changes

--- ansible/run-all.sh
+++ ansible/run-all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-. ./openrc.sh; ansible-playbook -vv -i inventory/inventory.ini main.yaml | tee output.txt
+. ./openrc.sh; ansible-playbook -vv -i inventory/inventory.ini --private-key=./shared-key.pem main.yaml | tee output.txt
